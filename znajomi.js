523d2e17653e1b557d6c821cffcf545bf0d60a3c
bartek8765 <bartekxd8@gmail.com>
2026-01-02 13:53:01 +0100

    Update znajomi.js

--- znajomi.js
+++ znajomi.js
@@ -11,9 +11,52 @@
 		    const pass = localStorage.getItem("password");
 	    	if(login && pass){
           document.getElementById("result").innerText = "Zalogowano jako: " + login;
+				wyswietl_znajomych();
         }
         else{
           document.getElementById("result").innerText = "Zaloguj się, aby wyświetlić znajomych";
         }
    
  };
+
+
+
+
+
+
+
+
+function wyswietl_znajomych() {
+const znajomi = document.getElementById("znajomi");
+
+
+
+	  fetch("https://cpp-b.onrender.com/wyswietl_znajomych", {
+                method: "POST",
+                headers: {
+                      "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    log: localStorage.getItem("login"),
+                    pass: localStorage.getItem("password")
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                
+				znajomi.innerHTML = "";
+				const lista_znajomych = data.lista_znajomych || [];
+				lista_znajomych.forEach(znajomy => { 
+					const li = document.createElement("li");
+        			li.textContent = znajomy;
+					znajomi.appendChild(li);
+				 });
+	
+        
+	})
+
+
+
+
+	
+};
