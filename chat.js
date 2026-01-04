6a59bbac9b624b2b793d149a1d3507bafaa06459
bartek8765 <bartekxd8@gmail.com>
2026-01-04 16:20:07 +0100

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+        document.getElementById("status").innerText = "Łączenie z serwerem...";
+        fetch("https://cpp-b.onrender.com/")
+        .then(res => res.text())
+        .then(text => document.getElementById("status").innerText = "Status serwera: " + text)
+        .catch(err => {console.error(err);
+                        document.getElementById("status").innerText = "Błąd połączenia z serwerem";
+                      });
+
+        const login = localStorage.getItem("login");
+		    const pass = localStorage.getItem("password");
+	    	if(login && pass){
+          document.getElementById("result").innerText = "Zalogowano jako: " + login;
+        }
+        else{
+          document.getElementById("result").innerText = "Zaloguj się, aby wyświetlić chat";
+        }
+   
+ };
